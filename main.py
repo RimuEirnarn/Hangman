60ef40e324ebcc57b3354f9841f10f07572f4239
RimuEirnarn <Rimuru720@Proton.me>
2022-11-26 11:51:07 +0700

    making it looks better thanks to pylint.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from os.path import exists, realpath
+"""Hangman"""
 from csv import reader, writer
-from string import punctuation
-from typing import Any, Callable, Optional, Union
-from argh import ArghParser, arg
+from os.path import exists, realpath
 from pathlib import Path
 from random import choice, randint
+from string import punctuation
+import sys
+from typing import Any, Callable, List, Optional, Tuple, Union
+from argh import ArghParser, arg
 
 DEFAULT_DATAPATH = Path(realpath(__file__+'/../data'))
 DEFAULT_WPATH = DEFAULT_DATAPATH / "word.csv"
@@ -20,59 +22,72 @@ a -> the guessed character
 BANNER = "Welcome to Hangman! (c) 2022 RimuEirnarn\n"
 HINT = (1, 2, 4)
 HINT_FLAG = (3, 7, 15)
+CLEAR = "\033[H\033[2J\033[3J"
+TupleAny = Tuple[Any, ...]
+ListTupleAny = List[TupleAny]
+TupleTupleAny = Tuple[Tuple[Any, ...]]
+AnyOrString = Union[Any, str]
+OptFunc = Optional[Callable[[Any], Union[Any, str]]]
+FailHook = Optional[Callable[[Any], Tuple[bool, str]]]
 
 
-def custom_input(prompt: str, default: str = "", hook: Optional[Callable[[Any], Union[Any, str]]] = None, fail_hook: Optional[Callable[[Any], tuple[bool, str]]] = None) -> Union[Any, str]:
+def custom_input(prompt: str,
+                 default: str = "",
+                 hook: OptFunc = None,
+                 fail_hook: FailHook = None) -> AnyOrString:
     """Prompt a input to user, return a 1-sized string or an empty string. It depends on hooks."""
     try:
-        rt = input(prompt).lower()
+        returns = input(prompt).lower()
         if fail_hook:
-            err = fail_hook(rt)
+            err = fail_hook(returns)
             if err[0]:
-                return hook(default if default == '' else default[0]) if hook is not None else default if default == '' else default[0]
+                return hook(default if default == '' else default[0]) if hook is not None \
+                    else default if default == '' else default[0]
         if hook:
-            return hook(rt)
-        return rt
+            return hook(returns)
+        return returns
     except (KeyboardInterrupt, IndexError):
         return default if default == '' else default[0]
     except EOFError:
-        exit(1)
+        sys.exit(1)
 
 
-def read_csv(filename: str, allrow: bool = False) -> Union[tuple[Any, ...], tuple[tuple[Any, ...]]]:
+def read_csv(filename: str, allrow: bool = False) -> Union[TupleAny, TupleTupleAny]:
     """Read a CSV file then return a tuple of tuples or just a tuple."""
-    with open(filename) as f:
-        rcsv = reader(f)
-        rt = []
+    with open(filename, encoding='utf-8') as file:
+        rcsv = reader(file)
+        returns = []
         for line in rcsv:
-            rt.append(tuple(line))
+            returns.append(tuple(line))
 
-        if len(rt) == 0:
+        if len(returns) == 0:
             raise ValueError("No data from CSV file is found.")
 
         if allrow:
-            return tuple(rt)
-        return rt[0]
+            return tuple(returns)
+        return returns[0]
 
 
-def write_csv(filename: str, data: Union[list[tuple[Any, ...]], tuple[Any, ...], tuple[tuple[Any, ...]]]):
+def write_csv(filename: str, data: Union[ListTupleAny, TupleAny, TupleTupleAny]):
     """Write a CSV data to a file."""
-    with open(filename, 'w') as f:
-        wcsv = writer(f)  # type: ignore
+    with open(filename, 'w', encoding='utf-8') as file:
+        writer_csv = writer(file)  # type: ignore
         if isinstance(data[0], (tuple, list)):
-            wcsv.writerows(data)
+            writer_csv.writerows(data)
         else:
-            wcsv.writerow(data)
+            writer_csv.writerow(data)
 
 
-def cast(data):
+def cast(data: Any):
+    """Cast a data ot either float, int or itself."""
     try:
         return float(data) if '.' in data else int(data)
-    except Exception:
+    except ValueError:
         return data
 
 
-def input_hook(data: str) -> tuple[Any, str]:
+def input_hook(data: str) -> AnyOrString:
+    """Input hook"""
     data = data.lower()
     if len(data) == 1:
         return 0, data
@@ -81,15 +96,15 @@ def input_hook(data: str) -> tuple[Any, str]:
     return 0, data[0]
 
 
-def padded_print(chrs, data):
+def padded_print(data):
     """Print stuff"""
-    pd = [1]
+    padded = [1]
     for idx in range(1, len(data)+1):
-        pd.append(len(str(idx)))
+        padded.append(len(str(idx)))
 
-    print(" ".join((a or "_"*pd[idx+1])+" "*(pd[idx+1]-1)
+    print(" ".join((a or "_"*padded[idx+1])+" "*(padded[idx+1]-1)
           for idx, a in enumerate(data)))
-    print(" ".join((str(d)+" "*(pd[idx+1]-1)
+    print(" ".join((str(d)+" "*(padded[idx+1]-1)
           for idx, d in enumerate(range(1, len(data)+1)))))
 
 
@@ -102,8 +117,8 @@ def main(word_list: Optional[str] = None):
 
     condition = None
     death = 3
-    fn_indexes = {}
-    # Set default data, will be allocated with None (and two helper hints) for the length of randomly selected word.
+    # Set default data, will be allocated with None (and two helper hints)
+    # for the length of randomly selected word.
     chars: list[Union[None, str]] = []
     words = read_csv(word_list or str(DEFAULT_WPATH))
 
@@ -119,19 +134,19 @@ def main(word_list: Optional[str] = None):
     chars.extend((None for _ in selected))
     passed = []
     while len(passed) != sel_hints:
-        x = randint(0, len(selected)-1)
-        if x in passed:
+        var_x = randint(0, len(selected)-1)
+        if var_x in passed:
             continue
-        passed.append(x)
-        chars[x] = selected[x]
-    print("\033[H\033[2J\033[3J" +
+        passed.append(var_x)
+        chars[var_x] = selected[var_x]
+    print(CLEAR +
           (BANNER_ONETIME if not exists(DEFAULT_DATAPATH / "init") else BANNER))
-    with open(DEFAULT_DATAPATH / "init", "w") as f:
-        pass
+    with open(DEFAULT_DATAPATH / "init", "w", encoding='utf-8') as file:
+        file.write('\0')
     turns = 0
     while True:
         # Print current status
-        padded_print(selected, chars)
+        padded_print(chars)
         print(
             f"You can play until {death} more mistake(s)" if death != 0 else "Game over!", end='')
 
@@ -159,13 +174,13 @@ def main(word_list: Optional[str] = None):
             break
 
         turns += 1
-        print("\33[H\033[2J\033[3J", end='')
+        print(CLEAR, end='')
 
-    print("\33[H\033[2J\033[3J", end='')
-    padded_print(selected, chars)
+    print(CLEAR, end='')
+    padded_print(chars)
     print(
         f"Correct word: {selected}\nGuessed totals: {''.join(((a or '_') for a in chars))}")
-    if condition == True:
+    if condition is True:
         print("\n\033[32mYou've won the game!\033[0m")
     else:
         print('\nYou lose, get better \033[32mnext\033[0m try!')
